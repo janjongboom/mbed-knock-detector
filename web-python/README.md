f3deaabedf46d234e5504e1be8769405470dac21
Jan Jongboom <janjongboom+mbed@gmail.com>
2016-02-28 17:57:25 +0000

    Update Python example with new host

--- web-python/README.md
+++ web-python/README.md
@@ -1,4 +1,7 @@
 # Installation
 
+* Go into 'mbed-connector-python' folder, and run `python setup.py` first
 * Run `pip install -U mdc_api`
 * Run `pip install pybars3`
+* Obtain an [access key](https://connector-test-sl.dev.mbed.com/#accesskeys)
+* Run `TOKEN=xxx python connector.py` (where xxx is your access key)
--- web-python/connector.py
+++ web-python/connector.py
@@ -6,6 +6,7 @@ import codecs
 import pprint
 import time
 import base64
+from threading import Timer
 from pybars import Compiler
 
 # map URL to class to handle requests
@@ -13,12 +14,13 @@ urls = (
     '/', 'index',
     '/knock-sensor/(.*)', 'knocksensor',
     '/api/knock-sensor/(.*)', 'apiknocksensor',
+    '/notification', 'notification'
 )
 
 token = os.environ['TOKEN'] # Get from connector.mbed.com
 if token == '':
-    raise Error('Missing token, run \'TOKEN=xxx python connector.py\'')
-connector = mdc_api.connector(token)
+    raise Exception('Missing token, run \'TOKEN=xxx python connector.py\'')
+connector = mdc_api.connector(token, 'https://ds-test-sl.dev.mbed.com')
 # connector.debug(True)
 compiler = Compiler() # pybars compiler
 
@@ -34,9 +36,18 @@ class index:
         while not e.isDone():
             None
         if e.error:
-            raise Error(e.error)
+            raise Exception(e.error.errType)
 
         return template({'endpoints': e.result})
+
+class notification:
+    # handle asynchronous events
+    def PUT(self):
+        if web.data: # verify there is data to process
+            print json.loads(web.data()).keys()
+            connector.handler(web.data()) # hand the data to the connector handler
+        return web.ok
+
 
 class knocksensor:
     def GET(self, id):
@@ -47,14 +58,14 @@ class knocksensor:
         while not e.isDone():
             time.sleep(1)
         if e.error:
-            raise Error(e.error)
+            raise Exception(e.error.errType)
 
         # also put a subscription for the resource in...
         r = connector.putResourceSubscription(id, KNOCK_RESOURCE)
         while not r.isDone():
             None
         if r.error:
-            raise Error(r.error)
+            raise Exception(r.error.errType)
 
         return template({'value': e.result, 'id': id})
 
@@ -65,7 +76,7 @@ class apiknocksensor:
         while not e.isDone():
             None
         if e.error:
-            raise Error(e.error)
+            raise Exception(e.error.errType)
         return e.result
 
 # 'notifications' are routed here
@@ -74,8 +85,20 @@ def notificationHandler(data):
     print "Notification Data Received: %s" %data['notifications']
     print "Notification Payload: %s" % base64.b64decode(data['notifications'][0]['payload'])
 
+def registerNotification():
+    p = connector.putCallback('http://' + os.environ['C9_HOSTNAME'] + '/notification')
+    while not p.isDone():
+        None
+    if p.error:
+        raise Exception(p.error.errType)
+    print "Callback URL is %s" % 'http://' + os.environ['C9_HOSTNAME'] + '/notification'
+
 if __name__ == "__main__":
-    e = connector.startLongPolling()
     connector.setHandler('notifications', notificationHandler) # send 'notifications' to the notificationHandler FN
+
+    # 2s after webpy starts we register notification
+    t = Timer(2, registerNotification)
+    t.start()
+
     app = web.application(urls, globals())
     app.run()
